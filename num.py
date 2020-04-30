4bfcc99ccdcfd02f8e76213ce804d83c923b1c14
Elvis Lam <elvis.hclam@yahoo.com.hk>
2020-04-30 15:26:27 +0800

    Final Version

--- num.py
+++ num.py
@@ -1,6 +1,12 @@
 import random
-r = random.randint(1,100)
+start = input('Please input Start number: ')
+end = input('Please input End number: ')
+start = int(start)
+end = int(end)
+r = random.randint(start,end)
+count = 0
 while True:
+	count += 1 #count = count + 1
 	num = input('Please enter a number: ')
 	num = int(num)
 	if num == r:
@@ -10,3 +16,5 @@ while True:
 			print('Your number is Bigger')
 	elif num < r:
 			print('Your number is Smaller')
+
+	print('This is your ', count, ' time')
